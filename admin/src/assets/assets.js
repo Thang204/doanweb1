096f89eb4a78e9b403ade6c6a49fffde0fb55e45
Thang <dogohoangphuc@gmail.com>
2025-04-01 20:45:09 +0700

    admin file

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,11 @@
+import logo from './logo.png'
+import add_icon from './add_icon.png'
+import orders from '../orders.png'
+import upload_area from './upload_area.png'
+
+export const assets = {
+    logo,
+    add_icon,
+    orders,
+    upload_area,
+}
